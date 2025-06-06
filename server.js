c553e7f53cc91c52c738b1fbf352237467a834bc
baran1234m <mehdi.rahmatolahi062@gmail.com>
2025-06-06 11:47:02 +0330

    fix url

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mafia';
-const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
+const BASE_URL = const BASE_URL = 'https://mafia-scenario-all.onrender.com';
+
 
 // اتصال به پایگاه داده
 mongoose.connect(MONGO_URI, {
